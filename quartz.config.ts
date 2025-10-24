f5df456a8c879c9e034d0f81758e5e1c991ddc67
pyorick <pyodep06@gmail.com>
2025-10-24 16:42:21 +0900

    update

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,9 +23,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Fira Sans",
-        body: "Roboto",
-        code: "Fira Code",
+        header: "Inter",
+        body: "Noto Sans KR",
+        code: "JetBrains Mono",
       },
       colors: {
         lightMode: {
