8cbf3765126301678032aeedd68b0d1e20ec3bba
pyorick <pyodep06@gmail.com>
2025-10-30 16:52:01 +0900

    index 홈화면 세팅2

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Home 🏠",
-    pageTitleSuffix: " | PYOdep's Blog",
+    pageTitleSuffix: " | PyOdep's Blog",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
